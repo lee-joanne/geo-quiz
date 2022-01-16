d840fead43b8bd7151fddc4badb47cf65633540a
Joanne <lee.joanne54@gmail.com>
2022-01-16 16:23:12 +0000

    Make individual DIVS in HTML for question area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+let start = document.getElementById("start");
+start.addEventListener("click", runGame);
+
+function runGame(event) {
+  
+}
+
 /* JavaScript code on modal window and close taken from W3 Schools page on modal windows: https://www.w3schools.com/howto/howto_css_modals.asp */
 let modal = document.getElementById("instructions-modal");
 let btn = document.getElementById("instructions");
