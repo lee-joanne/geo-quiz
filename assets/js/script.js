288f032f0f89258ef23305380086e77f68aef1b2
Joanne <lee.joanne54@gmail.com>
2022-01-20 18:31:41 +0000

    Make level one quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,49 @@ function runGame(levelOne){
   let questionArea = document.getElementById("question-area");
 }
 
-//let levelOne = 
+let levelOne = {
+  question: "What is the capital city of Germany?",
+  options: ["Berlin", "Hamburg", "Munich", "Frankfurt"],
+  answer: "Berlin",
 
-//research syntax for nested arrays
+  question: "Which country has the capital city of Canberra?",
+  options: ["New Zealand", "Australia", "Austria", "Slovakia"],
+  answer: "Australia",
+
+  question: "Which country has the Eiffel Tower?",
+  options: ["Germany", "France", "Portugal", "United Kingdom"],
+  answer: "France",
+
+  question: "In which Ukranian city did the 1986 Chernobyl Disaster take place?",
+  options: ["Lviv", "Dnipro", "Bakhmut", "Pripyat"],
+  answer: "Pripyat",
+
+  question: "How many provinces and territories are there in Canada?",
+  options: ["10 provinces 3 territories", "11 provinces 2 territories", "10 provinces 2 territories", "12 provinces 3 territories"],
+  answer: "10 provinces 3 territories",
+
+  question: "Which country has the flag with red and white stripes and fifty white stars?",
+  options: ["Algeria", "United States of America", "Moldova", "Russia"],
+  answer: "United States of America",
+
+  question: "Which of these countries are NOT part of the European Union?",
+  options: ["Malta", "Lithuania", "Romania", "Switzerland"],
+  answer: "Switzerland",
+
+  question: "Which country has the flag with the meaning 'circle of the sun'?",
+  options: ["Brazil", "Argentina", "Japan", "Laos"],
+  answer: "Japan",
+
+  question: "Which country has the most timezones?",
+  options: ["Russia", "France", "China", "Canada"],
+  answer: "France",
+
+  question: "Which city is right on the equator?",
+  options: ["Quito", "Cairo", "Kuala Lumpur", "Bangkok"],
+  answer: "Quito",
+}
+
+
 
 /* JavaScript code on modal window and close taken from W3 Schools page on modal windows: https://www.w3schools.com/howto/howto_css_modals.asp */
 let modal = document.getElementById("instructions-modal");
