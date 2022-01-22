bb11abdbcc321f8f95c7950df3875b51dfb64d18
Joanne <lee.joanne54@gmail.com>
2022-01-22 20:36:54 +0000

    Create for loop to make checkAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+let levelOneIndex = 0;
+//let levelTwoIndex = 0;
+//let levelThreeIndex = 0;
+
 let score = document.getElementById("scores");
 let startButton = document.getElementById("start");
 let choiceButtons = document.getElementsByClassName("choice-btn");
@@ -9,69 +13,128 @@ let submitButton = document.getElementById("submit");
 let gameOver = document.getElementById("game-over");
 let scores = document.getElementById("scores");
 let playAgainButton = document.getElementById("play-again");
+let questionText = document.getElementById("question-text");
 
 document.addEventListener('DOMContentLoaded', function () {
-  levelOneComplete.style.display = 'none';
-  levelTwoComplete.style.display = 'none';
-  levelThreeComplete.style.display = 'none';
-  submitButton.style.display = 'none';
-  gameOver.style.display = 'none';
-  scores.style.display = 'none';
-  playAgainButton.style.display = 'none';
+  levelOneComplete.classList.add('hide');
+  levelTwoComplete.classList.add('hide');
+  levelThreeComplete.classList.add('hide');
+  submitButton.classList.add('hide');
+  gameOver.classList.add('hide');
+  scores.classList.add('hide');
+  playAgainButton.classList.add('hide');
 });
 
-startButton.addEventListener("click", runGame);
+//add classes into html and .classList.remove after
+// style choice-btn css afterwards
 
+startButton.addEventListener("click", runLevelOne);
 
-function runGame(levelOne){
-let homepageContainer = document.getElementById("homepage-container");
-homepageContainer.classList.add('hide');
-question
+function runLevelOne(){
+//shufflequestions();
+  let homepageContainer = document.getElementById("homepage-container");
+  homepageContainer.classList.add('hide');
+  questionArea.classList.remove('hide');
+  // step 1: pull up the first question
+  let currentQuestion = levelOne[0];
+  console.table(currentQuestion);
 
+  // step 2: display question
+  questionText.textContent = currentQuestion.question;
+  // add style
+
+  console.log(currentQuestion.options);
+
+  function checkAnswer(e){
+    if (e.target.getAttribute('data-type') === currentQuestion.answer) {
+      alert("Good job! You got it right :)");
+    } else {
+      alert(`Sorry :( The answer was ${currentQuestion.answer}`);
+    }
+  }
+  //boolean 
+
+  for (let i = 0; i < choiceButtons.length; i++) {
+    choiceButtons[i].textContent = currentQuestion.options[i];
+    choiceButtons[i].setAttribute("data-type", currentQuestion.options[i]);
+    choiceButtons[i].addEventListener("click", checkAnswer);
+  }
 }
 
 
-let levelOne = {
-  question: "What is the capital city of Germany?",
-  options: ["Berlin", "Hamburg", "Munich", "Frankfurt"],
-  answer: "Berlin",
+  //choiceButtons.textContent = currentQuestion.options;
 
-  question: "Which country has the capital city of Canberra?",
-  options: ["New Zealand", "Australia", "Austria", "Slovakia"],
-  answer: "Australia",
 
-  question: "Which country has the Eiffel Tower?",
-  options: ["Germany", "France", "Portugal", "United Kingdom"],
-  answer: "France",
 
-  question: "In which Ukranian city did the 1986 Chernobyl Disaster take place?",
-  options: ["Lviv", "Dnipro", "Bakhmut", "Pripyat"],
-  answer: "Pripyat",
+  // step 3: select the button that you think is the answer
+  // step 4: click "submit"
+  // step 4.25: check answer 
+  // step 4.5: an alert at the top to validate whether answer is correct or not, with correct answer.
+  // steo 5: move onto the next question
+  // step 6: score updates
 
-  question: "How many provinces and territories are there in Canada?",
-  options: ["10 provinces 3 territories", "11 provinces 2 territories", "10 provinces 2 territories", "12 provinces 3 territories"],
-  answer: "10 provinces 3 territories",
 
-  question: "Which country has the flag with red and white stripes and fifty white stars?",
-  options: ["Algeria", "United States of America", "Moldova", "Russia"],
-  answer: "United States of America",
+let levelOne = [
+  { 
+    question: "What is the capital city of Germany?",
+    options: ["Berlin", "Hamburg", "Munich", "Frankfurt"],
+    answer: "Berlin",
+  },
 
-  question: "Which of these countries are NOT part of the European Union?",
-  options: ["Malta", "Lithuania", "Romania", "Switzerland"],
-  answer: "Switzerland",
+  {
+    question: "Which country has the capital city of Canberra?",
+    options: ["New Zealand", "Australia", "Austria", "Slovakia"],
+    answer: "Australia",
+  },
 
-  question: "Which country has the flag with the meaning 'circle of the sun'?",
-  options: ["Brazil", "South Korea", "Japan", "Laos"],
-  answer: "Japan",
+  { 
+    question: "Which country has the Eiffel Tower?",
+    options: ["Germany", "France", "Portugal", "United Kingdom"],
+    answer: "France",
+  },
 
-  question: "Which country has the most timezones?",
-  options: ["Russia", "France", "China", "Canada"],
-  answer: "France",
+  {
+    question: "In which Ukranian city did the 1986 Chernobyl Disaster take place?",
+    options: ["Lviv", "Dnipro", "Bakhmut", "Pripyat"],
+    answer: "Pripyat",
+  },
 
-  question: "Which city is right on the equator?",
-  options: ["Quito", "Cairo", "Kuala Lumpur", "Bangkok"],
-  answer: "Quito",
-}
+  {
+    question: "How many provinces and territories are there in Canada?",
+    options: ["10 provinces 3 territories", "11 provinces 2 territories", "10 provinces 2 territories", "12 provinces 3 territories"],
+    answer: "10 provinces 3 territories",
+  },
+
+  {
+    question: "Which country has the flag with red and white stripes and fifty white stars?",
+    options: ["Algeria", "United States of America", "Moldova", "Russia"],
+    answer: "United States of America",
+  },
+
+  {
+    question: "Which of these countries are NOT part of the European Union?",
+    options: ["Malta", "Lithuania", "Romania", "Switzerland"],
+    answer: "Switzerland",
+  },
+
+  {
+    question: "Which country has the flag with the meaning 'circle of the sun'?",
+    options: ["Brazil", "South Korea", "Japan", "Laos"],
+    answer: "Japan",
+  },
+
+  {
+    question: "Which country has the most timezones?",
+    options: ["Russia", "France", "China", "Canada"],
+    answer: "France",
+  },
+
+  {
+    question: "Which city is right on the equator?",
+    options: ["Quito", "Cairo", "Kuala Lumpur", "Bangkok"],
+    answer: "Quito",
+  },
+]
 
 
 /* JavaScript code on modal window and close taken from W3 Schools page on modal windows: https://www.w3schools.com/howto/howto_css_modals.asp */
