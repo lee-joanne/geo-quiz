e58e7d362a0f56c6c9ca5e358ab5d0859a081de2
Joanne <lee.joanne54@gmail.com>
2022-02-02 19:54:25 +0000

    Edit comment and docstrings of JS file to make functions more readable and undesrtandable

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,191 +16,192 @@ let feedbackText = document.getElementById("feedback-text");
 
 // Quiz questions for level one
 let levelOne = [{
-  question: "What is the capital city of Germany?",
-  options: ["Berlin", "Hamburg", "Munich", "Frankfurt"],
-  answer: "Berlin",
-},
+    question: "What is the capital city of Germany?",
+    options: ["Berlin", "Hamburg", "Munich", "Frankfurt"],
+    answer: "Berlin",
+  },
 
-{
-  question: "Which country has the capital city of Canberra?",
-  options: ["New Zealand", "Australia", "Austria", "Slovakia"],
-  answer: "Australia",
-},
+  {
+    question: "Which country has the capital city of Canberra?",
+    options: ["New Zealand", "Australia", "Austria", "Slovakia"],
+    answer: "Australia",
+  },
 
-{
-  question: "Which country has the Eiffel Tower?",
-  options: ["Germany", "France", "Portugal", "United Kingdom"],
-  answer: "France",
-},
+  {
+    question: "Which country has the Eiffel Tower?",
+    options: ["Germany", "France", "Portugal", "United Kingdom"],
+    answer: "France",
+  },
 
-{
-  question: "Which city used to be called Constantinople?",
-  options: ["Budapest", "Rome", "Istanbul", "Florence"],
-  answer: "Istanbul",
-},
+  {
+    question: "Which city used to be called Constantinople?",
+    options: ["Budapest", "Rome", "Istanbul", "Florence"],
+    answer: "Istanbul",
+  },
 
-{
-  question: "Which country is the famous archaeological site, Pompeii, in?",
-  options: ["Spain", "Croatia", "Italy", "Greece"],
-  answer: "Italy",
-},
+  {
+    question: "Which country is the famous archaeological site, Pompeii, in?",
+    options: ["Spain", "Croatia", "Italy", "Greece"],
+    answer: "Italy",
+  },
 
-{
-  question: "Which country has the flag with red and white stripes and fifty white stars?",
-  options: ["Algeria", "United States of America", "Moldova", "Russia"],
-  answer: "United States of America",
-},
+  {
+    question: "Which country has the flag with red and white stripes and fifty white stars?",
+    options: ["Algeria", "United States of America", "Moldova", "Russia"],
+    answer: "United States of America",
+  },
 
-{
-  question: "What is the capital city of Canada?",
-  options: ["Ottawa", "Toronto", "Vancouver", "Montréal"],
-  answer: "Ottawa",
-},
+  {
+    question: "What is the capital city of Canada?",
+    options: ["Ottawa", "Toronto", "Vancouver", "Montréal"],
+    answer: "Ottawa",
+  },
 
-{
-  question: "What is the smallest country in the world?",
-  options: ["Malta", "Vatican City", "Luxembourg", "Monaco"],
-  answer: "Vatican City",
-},
+  {
+    question: "What is the smallest country in the world?",
+    options: ["Malta", "Vatican City", "Luxembourg", "Monaco"],
+    answer: "Vatican City",
+  },
 
-{
-  question: "Which continent is Mexico in?",
-  options: ["South America", "Central America", "North America", "Europe"],
-  answer: "North America",
-},
+  {
+    question: "Which continent is Mexico in?",
+    options: ["South America", "Central America", "North America", "Europe"],
+    answer: "North America",
+  },
 
-{
-  question: "What is the capital city of Brazil?",
-  options: ["Brasília", "Rio de Janeiro", "São Paulo", "Manaus"],
-  answer: "Brasília",
-},
+  {
+    question: "What is the capital city of Brazil?",
+    options: ["Brasília", "Rio de Janeiro", "São Paulo", "Manaus"],
+    answer: "Brasília",
+  },
 ];
 
 // Quiz questions for level two
 let levelTwo = [{
-  question: "Which country has the flag with the meaning 'circle of the sun'?",
-  options: ["Brazil", "South Korea", "Japan", "Laos"],
-  answer: "Japan",
-},
+    question: "Which country has the flag with the meaning 'circle of the sun'?",
+    options: ["Brazil", "South Korea", "Japan", "Laos"],
+    answer: "Japan",
+  },
 
-{
-  question: "What is the official language of the United States?",
-  options: ["English", "English and Spanish", "English and French", "Nothing"],
-  answer: "Nothing",
-},
+  {
+    question: "What is the official language of the United States?",
+    options: ["English", "English and Spanish", "English and French", "Nothing"],
+    answer: "Nothing",
+  },
 
-{
-  question: "What are the official languages of Switzerland?",
-  options: ["German, French, English, Italian", "German and Italian", "German, French, Italian, Dutch", "German, French, Italian, Romansh"],
-  answer: "German, French, Italian, Romansh",
-},
+  {
+    question: "What are the official languages of Switzerland?",
+    options: ["German, French, English, Italian", "German and Italian", "German, French, Italian, Dutch", "German, French, Italian, Romansh"],
+    answer: "German, French, Italian, Romansh",
+  },
 
-{
-  question: "How many countries are in the continent of Africa?",
-  options: ["54", "45", "56", "48"],
-  answer: "54",
-},
+  {
+    question: "How many countries are in the continent of Africa?",
+    options: ["54", "45", "56", "48"],
+    answer: "54",
+  },
 
-{
-  question: "In which Ukranian city did the 1986 Chernobyl Disaster take place?",
-  options: ["Lviv", "Dnipro", "Bakhmut", "Pripyat"],
-  answer: "Pripyat",
-},
+  {
+    question: "In which Ukranian city did the 1986 Chernobyl Disaster take place?",
+    options: ["Lviv", "Dnipro", "Bakhmut", "Pripyat"],
+    answer: "Pripyat",
+  },
 
-{
-  question: "Which country is Norilsk, the most depressing city in the world, located?",
-  options: ["Ukraine", "Finland", "Belarus", "Russia"],
-  answer: "Russia",
-},
+  {
+    question: "Which country is Norilsk, the most depressing city in the world, located?",
+    options: ["Ukraine", "Finland", "Belarus", "Russia"],
+    answer: "Russia",
+  },
 
-{
-  question: "Which countries border the famous Iguazu Falls?",
-  options: ["Brazil and Argentina", "Brazil, Argentina, Paraquay", "Uruguay, Brazil, Argentina", "Chile and Argentina"],
-  answer: "Brazil, Argentina, Paraquay",
-},
+  {
+    question: "Which countries border the famous Iguazu Falls?",
+    options: ["Brazil and Argentina", "Brazil, Argentina, Paraquay", "Uruguay, Brazil, Argentina", "Chile and Argentina"],
+    answer: "Brazil, Argentina, Paraquay",
+  },
 
-{
-  question: "How many provinces and territories are there in Canada?",
-  options: ["10 provinces 3 territories", "11 provinces 2 territories", "10 provinces 2 territories", "12 provinces 3 territories"],
-  answer: "10 provinces 3 territories",
-},
+  {
+    question: "How many provinces and territories are there in Canada?",
+    options: ["10 provinces 3 territories", "11 provinces 2 territories", "10 provinces 2 territories", "12 provinces 3 territories"],
+    answer: "10 provinces 3 territories",
+  },
 
-{
-  question: "Which country borders fourteen countries but only has one time zone?",
-  options: ["Syria", "Mali", "Nepal", "China"],
-  answer: "China",
-},
+  {
+    question: "Which country borders fourteen countries but only has one time zone?",
+    options: ["Syria", "Mali", "Nepal", "China"],
+    answer: "China",
+  },
 
-{
-  question: "Which city is right on the equator?",
-  options: ["Quito", "Cairo", "Kuala Lumpur", "Bangkok"],
-  answer: "Quito",
-},
+  {
+    question: "Which city is right on the equator?",
+    options: ["Quito", "Cairo", "Kuala Lumpur", "Bangkok"],
+    answer: "Quito",
+  },
 ];
 
 // Quiz questions for level three
 let levelThree = [{
-  question: "Which country has the highest number of timezones?",
-  options: ["Russia", "France", "China", "Canada"],
-  answer: "France",
-},
+    question: "Which country has the highest number of timezones?",
+    options: ["Russia", "France", "China", "Canada"],
+    answer: "France",
+  },
 
-{
-  question: "Which countries make up the West Slavs?",
-  options: ["Poland, Czech Republic, Slovakia", "Russia, Belarus, Ukraine", "Poland, Bosnia, Croatia", "Slovakia, Slovenia, Austria"],
-  answer: "Poland, Czech Republic, Slovakia",
-},
+  {
+    question: "Which countries make up the West Slavs?",
+    options: ["Poland, Czech Republic, Slovakia", "Russia, Belarus, Ukraine", "Poland, Bosnia, Croatia", "Slovakia, Slovenia, Austria"],
+    answer: "Poland, Czech Republic, Slovakia",
+  },
 
-{
-  question: "How many states does Brazil have?",
-  options: ["28", "32", "34", "26"],
-  answer: "26",
-},
+  {
+    question: "How many states does Brazil have?",
+    options: ["28", "32", "34", "26"],
+    answer: "26",
+  },
 
-{
-  question: "Which of these countries all have flags of the sun?",
-  options: ["Argentina, Macedonia, Nepal, India", "Argentina, Bangladesh, Rwanda, and Uruguay", "Argentina, Niger, Congo, Somalia", "Argentina, Japan, Macedonia, Kenya"],
-  answer: "Argentina, Bangladesh, Rwanda, and Uruguay",
-},
+  {
+    question: "Which of these countries all have flags of the sun?",
+    options: ["Argentina, Macedonia, Nepal, India", "Argentina, Bangladesh, Rwanda, and Uruguay", "Argentina, Niger, Congo, Somalia", "Argentina, Japan, Macedonia, Kenya"],
+    answer: "Argentina, Bangladesh, Rwanda, and Uruguay",
+  },
 
-{
-  question: "Which of these countries all fall on the Tropic of Cancer?",
-  options: ["Egypt, Bolivia, Panama", "Mali, Sudan, Indonesia", "Mexico, Algeria, Libya", "India, Angola, Bahamas"],
-  answer: "Mexico, Algeria, Libya",
-},
+  {
+    question: "Which of these countries all fall on the Tropic of Cancer?",
+    options: ["Egypt, Bolivia, Panama", "Mali, Sudan, Indonesia", "Mexico, Algeria, Libya", "India, Angola, Bahamas"],
+    answer: "Mexico, Algeria, Libya",
+  },
 
-{
-  question: "Which of these countries is NOT part of the European Union?",
-  options: ["Malta", "Lithuania", "Romania", "Switzerland"],
-  answer: "Switzerland",
-},
+  {
+    question: "Which of these countries is NOT part of the European Union?",
+    options: ["Malta", "Lithuania", "Romania", "Switzerland"],
+    answer: "Switzerland",
+  },
 
-{
-  question: "Which country is Mount Kilimanjaro located in?",
-  options: ["Kenya", "Somalia", "Uganda", "Tanzania"],
-  answer: "Tanzania",
-},
+  {
+    question: "Which country is Mount Kilimanjaro located in?",
+    options: ["Kenya", "Somalia", "Uganda", "Tanzania"],
+    answer: "Tanzania",
+  },
 
-{
-  question: "How many states does Australia have?",
-  options: ["6", "5", "7", "8"],
-  answer: "6",
-},
+  {
+    question: "How many states does Australia have?",
+    options: ["6", "5", "7", "8"],
+    answer: "6",
+  },
 
-{
-  question: "Which of these islands are both part of French Polynesia?",
-  options: ["Bora Bora and Sumatra", "Bali and Tasmania", "Tahiti and Manihi", "Timor and Bonacca"],
-  answer: "Tahiti and Manihi",
-},
+  {
+    question: "Which of these islands are both part of French Polynesia?",
+    options: ["Bora Bora and Sumatra", "Bali and Tasmania", "Tahiti and Manihi", "Timor and Bonacca"],
+    answer: "Tahiti and Manihi",
+  },
 
-{
-  question: "Which country has the most islands?",
-  options: ["Canada", "Sweden", "Indonesia", "Greece"],
-  answer: "Sweden",
-},
+  {
+    question: "Which country has the most islands?",
+    options: ["Canada", "Sweden", "Indonesia", "Greece"],
+    answer: "Sweden",
+  },
 ];
 
 /* JavaScript code on modal window and close taken from W3 Schools page on modal windows: https://www.w3schools.com/howto/howto_css_modals.asp */
+// Modal window will open when user selects 'How To Play' button.
 let modal = document.getElementById("instructions-modal");
 let instructionsBtn = document.getElementById("instructions");
 let closeButton = document.getElementsByClassName("close")[0];
@@ -220,6 +221,7 @@ window.addEventListener("click", function (event) {
 });
 
 // This code on creating random numbers for the quiz to shuffle is mostly taken from Stack Overflow: https://stackoverflow.com/questions/18806210generating-non-repeating-random-numbers-in-js
+// Shuffles the quiz question array.
 let nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let ranNums = [];
 let i = nums.length;
@@ -234,16 +236,17 @@ while (i--) {
 let currentQuestionIndex = ranNums[0];
 let x = 1;
 
-// Event listener to run `runLevel()`.
+// Game will run when user clicks 'Let's Play' button. Runs `runLevel()`.
 startButton.addEventListener("click", runLevel);
+
 let currentQuestion = {};
 let currentRound = 0;
 
 /**
- * Initiates levels.
+ * Initiates game levels.
+ * Increments currentRound.
  * Runs `showGameBoard()`.
  * Runs `showQuestion()`.
- * Increments current round number.
  */
 function runLevel() {
   currentRound += 1;
@@ -253,8 +256,8 @@ function runLevel() {
 
 /**
  * Initiates the game.
- * Unhide questionArea and score.
- * Hide rest of elements.
+ * Unhides questionArea and scores.
+ * Hides rest of elements.
  */
 function showGameBoard() {
   homepageContainer.classList.add('hide');
@@ -268,11 +271,12 @@ function showGameBoard() {
 }
 
 /**
- * Game will loop through level one questions in the question array and the choices will iterate
- * through the multiple choice buttons.
+ * Switch statement will loop through different quiz questions depending on the currentRound.
+ * Question choices and question text from quiz questions inserted as text content
+ * When all ten questions have been iterated, runs `scoreRequirementConditionCheck()` and `resetQuestionIndex()`
+ * After, runs `enabledAndBindEventListenerChoiceButtons()`.
  */
 function showQuestions() {
-
   nextLevelButton.classList.add('hide');
   switch (currentRound) {
     case 1:
@@ -303,13 +307,13 @@ function showQuestions() {
 }
 
 /**
- * Checks the score of the game
- * If user scores equal to and above 6, user can proceed to level 2
- * If user scores equal to and above 12, user can proceed to level 3
- * If user scores equal to and above 20, user has completed the game
- * else, the game over message will show
+ * Switch statement which checks the score of the game.
+ * If user scores equal to and above 6, user can proceed to level 2.
+ * If user scores equal to and above 12, user can proceed to level 3.
+ * If user scores equal to and above 20, user has completed the game.
+ * Else, the game over message will show.
  */
-function scoreRequirementConditionCheck(){
+function scoreRequirementConditionCheck() {
   let score = parseInt(document.getElementById("score").innerText);
   switch (currentRound) {
     case 1:
@@ -320,26 +324,26 @@ function scoreRequirementConditionCheck(){
       }
       break;
     case 2:
-      if(score >= 12) {
+      if (score >= 12) {
         levelCompleted();
       } else {
         gameOver();
       }
       break;
     case 3:
-      if(score >= 20) {
+      if (score >= 20) {
         levelCompleted();
       } else {
         gameOver();
       }
       break;
   }
-  
+
 }
 
 /**
- * Enable the choiceButtons to be clickable.
- * Runs `checkAnswer()`.
+ * Enables the multiple choice options, choiceButtons, to be clickable.
+ * Event listener to run `checkAnswer()`.
  */
 function enabledAndBindEventListenerChoiceButtons() {
   for (let i = 0; i < choiceButtons.length; i++) {
@@ -350,8 +354,11 @@ function enabledAndBindEventListenerChoiceButtons() {
 
 /**
  * The user's input will be checked whether they have selected the correct or incorrect answer.
- * Correct answer shows encouraging feedback message.
+ * Correct answer shows the feedback message "correct".
  * Incorrect answer shows feedback message containing correct answer.
+ * Runs `counter()`.
+ * Runs `selectNextQuestion()`.
+ * Unhides nextButton.
  */
 function checkAnswer(e) {
   if (e.target.getAttribute('data-type') === currentQuestion.answer) {
@@ -369,7 +376,7 @@ function checkAnswer(e) {
 }
 
 /**
- * The score for the game will increment each time the user gets a correct answer.
+ * The score of the game will increment each time the user gets a correct answer.
  */
 function incrementScore() {
   let previousScore = parseInt(document.getElementById("score").innerText);
@@ -377,7 +384,7 @@ function incrementScore() {
 }
 
 /**
- * Increments current question index
+ * Increments currentQuestionIndex.
  */
 function counter() {
   currentQuestionIndex = ranNums[x];
@@ -385,20 +392,18 @@ function counter() {
 }
 
 /**
- * Function of EventListener of next button.
- * Runs `showLevelOneQuestions()`.
+ * nextButton event listener will run `showQuestions()`.
  */
 function selectNextQuestion() {
   nextButton.addEventListener("click", showQuestions);
 }
 
 /**
- * Question area is hidden.
- * Level complete messages appear and next level button will pop up.
- * Eventlistener runs `runLevel()`.
+ * Switch statement for when user successfully completes the current round.
+ * levelOneComplete and levelTwoComplete messages will show for case 1 and case 2, along with nextLevelButton to move onto the next round.
+ * `gameComplete()` will run for case 3 when user has completed the game.
  */
 function levelCompleted() {
-
   switch (currentRound) {
     case 1:
       questionArea.classList.add('hide');
@@ -414,11 +419,10 @@ function levelCompleted() {
       gameComplete();
       break;
   }
-  nextLevelButton.addEventListener("click", runLevel);
 }
 
 /**
- * Resets question index when level is complete
+ * Resets question index when level is complete.
  */
 function resetQuestionIndex() {
   x = 1;
@@ -426,8 +430,8 @@ function resetQuestionIndex() {
 }
 
 /**
- * Question area is hidden.
- * Game over message and play again button will pop up.
+ * Question area becomes hidden.
+ * gameOverMessage and playAgainButton will pop up.
  * Runs `playAgainEventListener()`.
  */
 function gameOver() {
@@ -438,12 +442,11 @@ function gameOver() {
 }
 
 /**
- * Question area is hidden.
- * Game complete message and play again button will pop up.
+ * Question area becomes hidden.
+ * LevelThreeComplete and playAgainButton will pop up.
  * Runs `playAgainEventListener()`.
  */
 function gameComplete() {
-  nextLevelButton.classList.add("hide");
   questionArea.classList.add('hide');
   levelThreeComplete.classList.remove('hide');
   playAgainButton.classList.remove('hide');
@@ -451,8 +454,7 @@ function gameComplete() {
 }
 
 /**
- * EventListener function for play again button.
- * Refreshes the page to restart score and shuffle
+ * playAgainButton will refresh the page when clicked.
  */
 function playAgainEventListenerRefresh() {
   playAgainButton.addEventListener('click', function () {
