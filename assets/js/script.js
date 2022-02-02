1430bea187849167035833900f69dd3c1bb45764
Joanne <lee.joanne54@gmail.com>
2022-02-02 18:13:18 +0000

    Add Beautify to JS, CSS, and HTML files. Create separate JS file for quiz questions and import onto HTML page

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,192 +14,6 @@ let questionText = document.getElementById("question-text");
 let nextButton = document.getElementById("next-button");
 let feedbackText = document.getElementById("feedback-text");
 
-// Quiz questions for level one
-let levelOne = [{
-    question: "What is the capital city of Germany?",
-    options: ["Berlin", "Hamburg", "Munich", "Frankfurt"],
-    answer: "Berlin",
-  },
-
-  {
-    question: "Which country has the capital city of Canberra?",
-    options: ["New Zealand", "Australia", "Austria", "Slovakia"],
-    answer: "Australia",
-  },
-
-  {
-    question: "Which country has the Eiffel Tower?",
-    options: ["Germany", "France", "Portugal", "United Kingdom"],
-    answer: "France",
-  },
-
-  {
-    question: "Which city used to be called Constantinople?",
-    options: ["Budapest", "Rome", "Istanbul", "Florence"],
-    answer: "Istanbul",
-  },
-
-  {
-    question: "Which country is the famous archaeological site, Pompeii, in?",
-    options: ["Spain", "Croatia", "Italy", "Greece"],
-    answer: "Italy",
-  },
-
-  {
-    question: "Which country has the flag with red and white stripes and fifty white stars?",
-    options: ["Algeria", "United States of America", "Moldova", "Russia"],
-    answer: "United States of America",
-  },
-
-  {
-    question: "What is the capital city of Canada?",
-    options: ["Ottawa", "Toronto", "Vancouver", "Montréal"],
-    answer: "Ottawa",
-  },
-
-  {
-    question: "What is the smallest country in the world?",
-    options: ["Malta", "Vatican City", "Luxembourg", "Monaco"],
-    answer: "Vatican City",
-  },
-
-  {
-    question: "Which continent is Mexico in?",
-    options: ["South America", "Central America", "North America", "Europe"],
-    answer: "North America",
-  },
-
-  {
-    question: "What is the capital city of Brazil?",
-    options: ["Brasília", "Rio de Janeiro", "São Paulo", "Manaus"],
-    answer: "Brasília",
-  },
-]
-
-// Quiz questions for level two
-let levelTwo = [{
-  question: "Which country has the flag with the meaning 'circle of the sun'?",
-  options: ["Brazil", "South Korea", "Japan", "Laos"],
-  answer: "Japan",
-  },
-
-  {
-    question: "What is the official language of the United States?",
-    options: ["English", "English and Spanish", "English and French", "Nothing"],
-    answer: "Nothing",
-  },
-
-  {
-    question: "What are the official languages of Switzerland?",
-    options: ["German, French, English, Italian", "German and Italian", "German, French, Italian, Dutch", "German, French, Italian, Romansh"],
-    answer: "German, French, Italian, Romansh",
-  },
-
-  {
-    question: "How many countries are in the continent of Africa?",
-    options: ["54", "45", "56", "48"],
-    answer: "54",
-  },
-
-  {
-    question: "In which Ukranian city did the 1986 Chernobyl Disaster take place?",
-    options: ["Lviv", "Dnipro", "Bakhmut", "Pripyat"],
-    answer: "Pripyat",
-  },
-
-  {
-    question: "Which country is Norilsk, the most depressing city in the world, located?",
-    options: ["Ukraine", "Finland", "Belarus", "Russia"],
-    answer: "Russia",
-  },
-
-  {
-    question: "Which countries border the famous Iguazu Falls?",
-    options: ["Brazil and Argentina", "Brazil, Argentina, Paraquay", "Uruguay, Brazil, Argentina", "Chile and Argentina"],
-    answer: "Brazil, Argentina, Paraquay",
-  },
-
-  {
-    question: "How many provinces and territories are there in Canada?",
-    options: ["10 provinces 3 territories", "11 provinces 2 territories", "10 provinces 2 territories", "12 provinces 3 territories"],
-    answer: "10 provinces 3 territories",
-  },
-
-  {
-    question: "Which country borders fourteen countries but only has one time zone?",
-    options: ["Syria", "Mali", "Nepal", "China"],
-    answer: "China",
-  },
-
-  {
-    question: "Which city is right on the equator?",
-    options: ["Quito", "Cairo", "Kuala Lumpur", "Bangkok"],
-    answer: "Quito",
-  },
-]
-
-// Quiz questions for level three
-let levelThree = [{
-  question: "Which country has the highest number of timezones?",
-  options: ["Russia", "France", "China", "Canada"],
-  answer: "France",
-},
-
-{
-  question: "Which countries make up the West Slavs?",
-  options: ["Poland, Czech Republic, Slovakia", "Russia, Belarus, Ukraine", "Poland, Bosnia, Croatia", "Slovakia, Slovenia, Austria"],
-  answer: "Poland, Czech Republic, Slovakia",
-},
-
-{
-  question: "How many states does Brazil have?",
-  options: ["28", "32", "34", "26"],
-  answer: "26",
-},
-
-{
-  question: "Which of these countries all have flags of the sun?",
-  options: ["Argentina, Macedonia, Nepal, India", "Argentina, Bangladesh, Rwanda, and Uruguay", "Argentina, Niger, Congo, Somalia", "Argentina, Japan, Macedonia, Kenya"],
-  answer: "Argentina, Bangladesh, Rwanda, and Uruguay",
-},
-
-{
-  question: "Which of these countries all fall on the Tropic of Cancer?",
-  options: ["Egypt, Bolivia, Panama", "Mali, Sudan, Indonesia", "Mexico, Algeria, Libya", "India, Angola, Bahamas"],
-  answer: "Mexico, Algeria, Libya",
-},
-
-{
-  question: "Which of these countries is NOT part of the European Union?",
-  options: ["Malta", "Lithuania", "Romania", "Switzerland"],
-  answer: "Switzerland",
-},
-
-{
-  question: "Which country is Mount Kilimanjaro located in?",
-  options: ["Kenya", "Somalia", "Uganda", "Tanzania"],
-  answer: "Tanzania",
-},
-
-{
-  question: "How many states does Australia have?",
-  options: ["6", "5", "7", "8"],
-  answer: "6",
-},
-
-{
-  question: "Which of these islands are both part of French Polynesia?",
-  options: ["Bora Bora and Sumatra", "Bali and Tasmania", "Tahiti and Manihi", "Timor and Bonacca"],
-  answer: "Tahiti and Manihi",
-},
-
-{
-  question: "Which country has the most islands?",
-  options: ["Canada", "Sweden", "Indonesia", "Greece"],
-  answer: "Sweden",
-},
-]
-
 /* JavaScript code on modal window and close taken from W3 Schools page on modal windows: https://www.w3schools.com/howto/howto_css_modals.asp */
 let modal = document.getElementById("instructions-modal");
 let instructionsBtn = document.getElementById("instructions");
@@ -377,8 +191,9 @@ function levelCompleted() {
       break;
     case 3:
       gameComplete();
-      break;}
-    nextLevelButton.addEventListener("click", runLevel);
+      break;
+  }
+  nextLevelButton.addEventListener("click", runLevel);
 }
 
 /**
@@ -421,5 +236,7 @@ function gameComplete() {
  * Refreshes the page to restart score and shuffle
  */
 function playAgainEventListenerRefresh() {
-  playAgainButton.addEventListener('click', function(){location.reload()});
+  playAgainButton.addEventListener('click', function () {
+    location.reload()
+  });
 }
